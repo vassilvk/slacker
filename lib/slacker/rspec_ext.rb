470a54f71516c03100d14a6c421deabae63c9fc0
Vassil Kovatchev <vassil.kovatchev@gmail.com>
2015-11-04 13:56:59 -0500

    Added comments to the DSL extension methods

--- lib/slacker/rspec_ext.rb
+++ lib/slacker/rspec_ext.rb
@@ -4,6 +4,9 @@ require 'csv'
 
 module Slacker
   module RSpecExt
+    
+    # Execute a SQL query or the contents of a query template query file
+    # In case of a query file, it passes the options to the template
     def query(query_string, options = {}, log_name = nil)
       log_name ||= Slacker.construct_log_name('query', query_string, options)
       sql = Slacker.sql_from_query_string(query_string, options)
@@ -14,6 +17,7 @@ module Slacker
       @results
     end
 
+    # Exposes a reference to the sql object, which represents the contents of the SQL folder
     def sql
       Slacker.sql(self)
     end
@@ -23,14 +27,18 @@ module Slacker
       QueryResultMatcher.new(Slacker.filter_golden_master(golden_master))
     end
 
+    # Loads a CSV file and returns a reference to it
+    # The file is relative to the data folder of the Slacker project
     def csv(csv_file)
       Slacker.get_csv(csv_file)
     end
 
+    # Generate CSV::Table objects with an arbitrary number of records populated with data based on a template CSV file
     def touch_csv(csv_file_or_object, fields, field_generators = {})
       Slacker.touch_csv(csv_file_or_object, fields, field_generators)
     end
 
+    # Load a CSV file directly into a table in the target database
     def load_csv(csv_file_or_object, table_name, log_name = nil)
       log_name ||= "load_csv '#{csv_file_or_object.kind_of?(CSV::Table) ? 'CSV Object' : csv_file_or_object }', 'table_name'"
       csv_object = case csv_file_or_object
