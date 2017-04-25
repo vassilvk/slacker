ab409f4043e136b71cb37cb70fa0843b8e916d87
Vassil Kovatchev <vkovatchev@deloitte.com>
2017-04-25 15:57:10 -0400

    Upgrade RSpec tests to RSpec 3.x

--- spec/query_result_matcher_spec.rb
+++ spec/query_result_matcher_spec.rb
@@ -19,14 +19,14 @@ describe Slacker::QueryResultMatcher do
       one_column_too_many = deep_copy(@subject).each{|row| row['new column'] = 'val 1'}
       wrong_column_name = deep_copy(@subject).each{|row| row.delete('Field_2'); row['Field_x'] = 'val x'}
 
-      @matcher.matches?(one_column_too_few).should be_false
-      @matcher.failure_message_for_should.should == 'Expected 3 field(s), got 2'
+      @matcher.matches?(one_column_too_few).should be false
+      @matcher.failure_message.should == 'Expected 3 field(s), got 2'
 
-      @matcher.matches?(one_column_too_many).should be_false
-      @matcher.failure_message_for_should.should == 'Expected 3 field(s), got 4'
+      @matcher.matches?(one_column_too_many).should be false
+      @matcher.failure_message.should == 'Expected 3 field(s), got 4'
 
-      @matcher.matches?(wrong_column_name).should be_false
-      @matcher.failure_message_for_should.should == 'Expected field "Field_2", got field "b"'
+      @matcher.matches?(wrong_column_name).should be false
+      @matcher.failure_message.should == 'Expected field "Field_2", got field "b"'
     end
 
     it 'correctly rejects a non-matching query result based on number of records' do
@@ -35,14 +35,14 @@ describe Slacker::QueryResultMatcher do
       one_row_too_many = deep_copy(@subject) << @subject[0]
       empty_query_result = []
 
-      @matcher.matches?(one_row_too_few).should be_false
-      @matcher.failure_message_for_should.should == 'Expected 2 record(s), got 1'
+      @matcher.matches?(one_row_too_few).should be false
+      @matcher.failure_message.should == 'Expected 2 record(s), got 1'
 
-      @matcher.matches?(one_row_too_many).should be_false
-      @matcher.failure_message_for_should.should == 'Expected 2 record(s), got 3'
+      @matcher.matches?(one_row_too_many).should be false
+      @matcher.failure_message.should == 'Expected 2 record(s), got 3'
 
-      @matcher.matches?(empty_query_result).should be_false
-      @matcher.failure_message_for_should.should == 'Expected 2 record(s), got 0'
+      @matcher.matches?(empty_query_result).should be false
+      @matcher.failure_message.should == 'Expected 2 record(s), got 0'
     end
 
     it 'correctly rejects a non-matching query result based on a value' do
@@ -64,62 +64,62 @@ describe Slacker::QueryResultMatcher do
       misplaced_fields[0]['Field 1'] = 12
       misplaced_fields[1]['Field 1'] = 'test string'
 
-      @matcher.matches?(wrong_int).should be_false
-      @matcher.failure_message_for_should.should == 'Field "Field 1", Record 1: Expected value "12", got "14"'
+      @matcher.matches?(wrong_int).should be false
+      @matcher.failure_message.should == 'Field "Field 1", Record 1: Expected value "12", got "14"'
 
 
-      @matcher.matches?(wrong_int_type).should be_false
-      @matcher.failure_message_for_should.should == 'Field "Field 1", Record 2: Expected value "test string", got "14"'
+      @matcher.matches?(wrong_int_type).should be false
+      @matcher.failure_message.should == 'Field "Field 1", Record 2: Expected value "test string", got "14"'
 
-      @matcher.matches?(wrong_nil).should be_false
-      @matcher.failure_message_for_should.should == 'Field "Field 1", Record 1: Expected value "12", got <NULL>'
+      @matcher.matches?(wrong_nil).should be false
+      @matcher.failure_message.should == 'Field "Field 1", Record 1: Expected value "12", got <NULL>'
 
-      @matcher.matches?(wrong_non_nil).should be_false
-      @matcher.failure_message_for_should.should == 'Field "Field_2", Record 1: Expected value <NULL>, got "whatever"'
+      @matcher.matches?(wrong_non_nil).should be false
+      @matcher.failure_message.should == 'Field "Field_2", Record 1: Expected value <NULL>, got "whatever"'
 
-      @matcher.matches?(misplaced_fields).should be_false
-      @matcher.failure_message_for_should.should == 'Expected field "Field 1", got field "Field_2"'
+      @matcher.matches?(misplaced_fields).should be false
+      @matcher.failure_message.should == 'Expected field "Field 1", got field "Field_2"'
     end
 
     it 'correctly compares with a query result' do
-      @matcher.matches?(@subject).should be_true(@matcher.failure_message_for_should)
+      @matcher.matches?(@subject).should be true
     end
 
     it 'only accepts well formed query result subjects' do
       expected_message = /Can perform query matches only against a well formed query result subject/
-      @matcher.matches?(1).should be_false
-      @matcher.failure_message_for_should.should =~ expected_message
+      @matcher.matches?(1).should be false
+      @matcher.failure_message.should =~ expected_message
 
-      @matcher.matches?('test string').should be_false
-      @matcher.failure_message_for_should.should =~ expected_message
+      @matcher.matches?('test string').should be false
+      @matcher.failure_message.should =~ expected_message
 
-      @matcher.matches?(1.1).should be_false
-      @matcher.failure_message_for_should.should =~ expected_message
+      @matcher.matches?(1.1).should be false
+      @matcher.failure_message.should =~ expected_message
 
-      @matcher.matches?(Time.now).should be_false
-      @matcher.failure_message_for_should.should =~ expected_message
+      @matcher.matches?(Time.now).should be false
+      @matcher.failure_message.should =~ expected_message
 
-      @matcher.matches?(nil).should be_false
-      @matcher.failure_message_for_should.should =~ expected_message
+      @matcher.matches?(nil).should be false
+      @matcher.failure_message.should =~ expected_message
 
-      @matcher.matches?([{:f1 => 'x', :f2 => 'y'}, {:f1 => 'x'}]).should be_false
-      @matcher.failure_message_for_should.should =~ expected_message
+      @matcher.matches?([{:f1 => 'x', :f2 => 'y'}, {:f1 => 'x'}]).should be false
+      @matcher.failure_message.should =~ expected_message
 
-      @matcher.matches?([{:f1 => 'x', :f2 => 'y'}, {:f1 => 'x', :f3 => 'y'}]).should be_false
-      @matcher.failure_message_for_should.should =~ expected_message
+      @matcher.matches?([{:f1 => 'x', :f2 => 'y'}, {:f1 => 'x', :f3 => 'y'}]).should be false
+      @matcher.failure_message.should =~ expected_message
 
-      @matcher.matches?([{:f1 => 'x', :f2 => 'y'}, {:f1 => 'x', :f2 => 'y', :f3 => 'z'}]).should be_false
-      @matcher.failure_message_for_should.should =~ expected_message
+      @matcher.matches?([{:f1 => 'x', :f2 => 'y'}, {:f1 => 'x', :f2 => 'y', :f3 => 'z'}]).should be false
+      @matcher.failure_message.should =~ expected_message
 
-      @matcher.matches?([{:f1 => 'x', :f2 => 'y'}, 12]).should be_false
-      @matcher.failure_message_for_should.should =~ expected_message
+      @matcher.matches?([{:f1 => 'x', :f2 => 'y'}, 12]).should be false
+      @matcher.failure_message.should =~ expected_message
 
-      @matcher.matches?([12, {:f1 => 'x', :f2 => 'y'}]).should be_false
-      @matcher.failure_message_for_should.should =~ expected_message
+      @matcher.matches?([12, {:f1 => 'x', :f2 => 'y'}]).should be false
+      @matcher.failure_message.should =~ expected_message
 
       # An array with a single empty row is not a well-formed query result
-      @matcher.matches?([{}]).should be_false
-      @matcher.failure_message_for_should.should =~ expected_message
+      @matcher.matches?([{}]).should be false
+      @matcher.failure_message.should =~ expected_message
     end
     
   end
@@ -133,10 +133,10 @@ describe Slacker::QueryResultMatcher do
 
     it 'correctly compares an empty subject with an empty CSV file' do
       matcher = Slacker::QueryResultMatcher.new(SpecHelper.load_csv('matcher/no_rows.csv'))
-      matcher.matches?([]).should be_true(matcher.failure_message_for_should)
+      matcher.matches?([]).should be true
 
       matcher = Slacker::QueryResultMatcher.new(SpecHelper.load_csv('matcher/completely_blank.csv'))
-      matcher.matches?([]).should be_true(matcher.failure_message_for_should)
+      matcher.matches?([]).should be true
     end
   end
 
@@ -151,50 +151,50 @@ describe Slacker::QueryResultMatcher do
       expected_message = /Cannot match against a non-well formed golden master array/
 
       matcher = Slacker::QueryResultMatcher.new([{:f1 => 'x', :f2 => 'y'}, {:f1 => 'x'}])
-      matcher.matches?([]).should be_false
-      matcher.failure_message_for_should.should =~ expected_message
+      matcher.matches?([]).should be false
+      matcher.failure_message.should =~ expected_message
 
       matcher = Slacker::QueryResultMatcher.new([{:f1 => 'x', :f2 => 'y'}, {:f1 => 'x', :f3 => 'y'}])
-      matcher.matches?([]).should be_false
-      matcher.failure_message_for_should.should =~ expected_message
+      matcher.matches?([]).should be false
+      matcher.failure_message.should =~ expected_message
 
       matcher = Slacker::QueryResultMatcher.new([{:f1 => 'x', :f2 => 'y'}, {:f1 => 'x', :f2 => 'y', :f3 => 'z'}])
-      matcher.matches?([]).should be_false
-      matcher.failure_message_for_should.should =~ expected_message
+      matcher.matches?([]).should be false
+      matcher.failure_message.should =~ expected_message
 
       matcher = Slacker::QueryResultMatcher.new([{:f1 => 'x', :f2 => 'y'}, 12])
-      matcher.matches?([]).should be_false
-      matcher.failure_message_for_should.should =~ expected_message
+      matcher.matches?([]).should be false
+      matcher.failure_message.should =~ expected_message
 
       matcher = Slacker::QueryResultMatcher.new([12, {:f1 => 'x', :f2 => 'y'}])
-      matcher.matches?([]).should be_false
-      matcher.failure_message_for_should.should =~ expected_message
+      matcher.matches?([]).should be false
+      matcher.failure_message.should =~ expected_message
     end
 
     it 'correctly compares an empty subject with an empty golden master' do
       matcher = Slacker::QueryResultMatcher.new([])
-      matcher.matches?([]).should be_true(matcher.failure_message_for_should)
+      matcher.matches?([]).should be true
     end
   end
 
   shared_examples_for 'single-value-based matcher' do
     it 'should correctly match the first value in the result set' do
       matcher = Slacker::QueryResultMatcher.new(@correct_golden_master)
-      matcher.matches?(@subject).should be_true(matcher.failure_message_for_should)
+      matcher.matches?(@subject).should be true
     end
 
     it 'should correctly reject a non-matching first item by value' do
       if !@wrong_value_golden_master.nil? #Skip nil from value check when the master is nil
         matcher = Slacker::QueryResultMatcher.new(@wrong_value_golden_master)
-        matcher.matches?(@subject).should be_false
-        matcher.failure_message_for_should.should =~ /Expected value "#{@wrong_value_golden_master}", got "#{@subject[0].values[0]}"/
+        matcher.matches?(@subject).should be false
+        matcher.failure_message.should =~ /Expected value "#{@wrong_value_golden_master}", got "#{@subject[0].values[0]}"/
       end
     end
 
     it 'should correctly reject a non-matching first item by type' do
       matcher = Slacker::QueryResultMatcher.new(@wrong_type_golden_master)
-      matcher.matches?(@subject).should be_false
-      matcher.failure_message_for_should.should =~ /Expected type "#{@wrong_type_golden_master.class}", got \"#{@subject[0].values[0].class}\"/
+      matcher.matches?(@subject).should be false
+      matcher.failure_message.should =~ /Expected type "#{@wrong_type_golden_master.class}", got \"#{@subject[0].values[0].class}\"/
     end
   end
 
@@ -256,13 +256,13 @@ describe Slacker::QueryResultMatcher do
   it 'correctly matches a multi-result subject' do
     subject = [@subject, [{:Field_x => 12}]]
     matcher = Slacker::QueryResultMatcher.new(deep_copy(@subject))
-    matcher.matches?(subject).should be_true(matcher.failure_message_for_should)
+    matcher.matches?(subject).should be true
   end
 
   it 'correctly rejects a wrong multi-result subject' do
     subject = [[{:Field_x => 12}], @subject]
     matcher = Slacker::QueryResultMatcher.new(deep_copy(@subject))
-    matcher.matches?(subject).should be_false
-    matcher.failure_message_for_should.should == 'Expected 3 field(s), got 1'
+    matcher.matches?(subject).should be false
+    matcher.failure_message.should == 'Expected 3 field(s), got 1'
   end
 end
